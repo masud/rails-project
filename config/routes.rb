fe71f8b615233245550c957c12df0fa2a997a3c7
Masud Ahmed <masud@karitkarma.com>
2015-03-16 23:24:20 +0600

    static page finised

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  root 'static_pages#home'
 
   get 'static_pages/help'
 
+  get 'static_pages/about'
+
   resources :microposts
-  root 'users#index'
+  # root 'users#index'
   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
